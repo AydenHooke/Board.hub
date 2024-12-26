220479452bd4ac556fcb648d9b82d5b2593f7d82
Cullen Vaughan <cullenvaughan@proton.me>
2024-12-26 15:54:10 -0600

    Added Forums.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
 import SignInLogic from './Components/SignIn/SignInLogic'
 import SignUpLogic from './Components/SignUp/SignUpLogic'
+import Forums from './Components/Forums/Forums';
 
 function App() {
   return (
@@ -9,8 +11,10 @@ function App() {
       <BrowserRouter>
         <NavBar/>
         <Routes>
+          <Route path='/' element={<Home/>}></Route>
           <Route path='/SignUp' element={<SignUpLogic/>}></Route>
           <Route path='/SignIn' element={<SignInLogic/>}></Route>
+          <Route path='/Forums' element={<Forums/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
