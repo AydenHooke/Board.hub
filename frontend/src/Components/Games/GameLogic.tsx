31d923d7ce717f1b47edb4562c301acd81ec7466
denprud <denzelprud1@gmail.com>
2024-12-26 16:48:57 -0500

    Created game page skeleton

---
+++ frontend/src/Components/Games/GameLogic.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function GameLogic() {
+
+    function handleGetGameCollection() {
+        // Get the Game Collection from BGG
+    }
+
+  return (
+    <>
+        <button onClick={handleGetGameCollection}>
+            Get Game Collection From BGG
+        </button>
+    </>
+  )
+}
+
+export default GameLogic
