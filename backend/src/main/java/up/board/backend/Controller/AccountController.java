b74ece303fccef4f59fefffa778c2e2bb12e7e07
Thomas Sullivan <thomassullivanjames@live.com>
2024-12-26 16:30:47 -0500

    user register + .gitignore in project root

--- backend/src/main/java/up/board/backend/Controller/AccountController.java
+++ backend/src/main/java/up/board/backend/Controller/AccountController.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import up.board.backend.Entity.Account;
+import up.board.backend.Service.AccountService;
 
 @RestController
 @RequestMapping("/account")
@@ -15,13 +19,37 @@ public class AccountController {
 
   private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+  AccountService accountService;
+
+  //
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
   /// Endpoints
-  @GetMapping("/index")
-  public ResponseEntity<String> index() {
+  @PostMapping("/register")
+  public ResponseEntity<String> register(@RequestBody Account account) {
 
-    logger.info("User visited index");
+    // Check valid username
+    var username = account.getUsername();
+    if (username == null || username.length() == 0) {
+      return ResponseEntity.status(409).body("Invalid username");
+    }
 
-    return ResponseEntity.ok().body("Hello!");
+    // Make sure username is not in use
+    var existingAccount = accountService.FindByUsername(account.getUsername());
+    if (existingAccount != null) {
+      return ResponseEntity.status(409).body("Username taken");
+    }
+
+    // Encode password
+    var encodedPassword = AccountService.GetEncodedPassword(account.getPasswordHash());
+    account.setPasswordHash(encodedPassword);
+
+    accountService.Register(account);
+
+    // Return OK
+    return ResponseEntity.ok().body("Yup");
   }
 
 }
