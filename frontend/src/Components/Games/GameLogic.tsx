4bc23d9beb1f2f9e45c61c1e352c06da5f4618c1
Drsen57 <ayden.hooke@gmail.com>
2024-12-27 13:57:06 -0500

    attempt xml to json

--- frontend/src/Components/Games/GameLogic.tsx
+++ frontend/src/Components/Games/GameLogic.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function GameLogic() {
+  const [state, setState] = useState({
+    collection: {} as any
+  });
 
-    function handleGetGameCollection() {
-        // Get the Game Collection from BGG
+  let xml2js = require('xml2js');
+
+  useEffect(()=>{
+    let jString;
+    let parser = new xml2js.Parser();
+    parser.parseStringPromise(state.collection).then(function (result: any) {
+      console.dir(result)
+      console.log('Done')
+      JSON.stringify(result);
+    })
+
+    // let games = state.collection.getElementsByTagName('item');
+    // let numberOfGames = state.collection.getElementsByTagName('item').length;
+    // let ids:number[] = [];
+
+    // for(let i=0;i<numberOfGames;i++)
+    //   ids[i] = games[i].getElementsByTagName('id')[0].innerHTML;
+
+      console.log(jString);
+  }, [state])
+  
+
+  let username = "Drsen57";
+
+
+    async function handleGetGameCollection() {
+        let myCollection = await axios.get(`https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&subtype=boardgame&own=1`)
+        const collectionData = myCollection.data;
+        //console.log(myCollection.headers); it automatically parses it as xml
+        setState(()=>({collection:collectionData}));
+        
     }
 
   return (
