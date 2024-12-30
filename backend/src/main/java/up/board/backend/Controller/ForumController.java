f3f9ed876cadd0f362ba3a77c906a33d3df9d9c0
Thomas Sullivan <thomassullivanjames@live.com>
2024-12-30 11:48:05 -0500

    forum, thread, reply controllers and services

--- backend/src/main/java/up/board/backend/Controller/ForumController.java
+++ backend/src/main/java/up/board/backend/Controller/ForumController.java
@@ -1,17 +1,11 @@
 package up.board.backend.Controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import up.board.backend.Entity.Thread;
 import up.board.backend.Service.ForumService;
 
 @RestController
@@ -29,12 +23,6 @@ public class ForumController {
   }
 
   /// Endpoints
-  @GetMapping("/get/{forumId}")
-  public ResponseEntity<List<Thread>> getThreads(@PathVariable Integer forumId) {
 
-    // Return threads for forum
-    var threads = forumService.getThreads(forumId);
-    return ResponseEntity.ok().body(threads);
-  }
 
 }
