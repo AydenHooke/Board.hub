26a2980aa0b5fb4c46808375ffe0ee8aef8476f7
Cullen Vaughan <nelluc464@gmail.com>
2025-01-03 15:24:52 -0600

    added reload to forums

--- frontend/src/Components/Forums/ForumPage.tsx
+++ frontend/src/Components/Forums/ForumPage.tsx
@@ -23,18 +23,22 @@ function ForumPage({
 
     const [data, setData] = useState<Thread[]>([]);
     const [threadId, setThreadId] = useState(-1);
+    const [reload, setReload] = useState(false);
 
     useEffect(() => {
+        setReload(false);
         axios
             .get('http://localhost:8080/thread/get/' + forumId)
             .then((Response) => setData(Response.data))
             .catch((error) => console.error('Error getting data, ', error));
-    }, [])
+    }, [reload])
 
     return (
         <>
             {(threadId == -1) && <h2>{title}</h2>}
             {(threadId == -1) && <h3>{description}</h3>}
+
+            {(threadId == -1) && <button onClick={(e: any) => setReload(true)}>Reload</button>}
 
             {
                 (context?.id != '' && threadId == -1) && <CreateThreadLogic
