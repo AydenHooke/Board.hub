c908f675fced5333c8edf139f962b9279d6e8299
Thomas Sullivan <thomassullivanjames@live.com>
2025-01-03 16:20:38 -0500

    return username with reply and thread + test fixes

--- backend/src/main/java/up/board/backend/Repository/ReplyRepository.java
+++ backend/src/main/java/up/board/backend/Repository/ReplyRepository.java
@@ -3,8 +3,10 @@ package up.board.backend.Repository;
 import up.board.backend.Entity.Reply;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,6 +15,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Integer> {
   public Reply getReplyByReplyId(int replyId);
 
   //
-  public List<Reply> getReplyByThreadId(int threadId);
+  @Query(value = "SELECT r.*, a.* FROM Reply r INNER JOIN Account a ON (r.account_id = a.account_id) WHERE r.thread_id = :threadId", nativeQuery = true)
+  public List<Map<String, Object>> getReplyByThreadId(int threadId);
 
 }
