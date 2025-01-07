e4e2a31a3569e15802bb4eb1ec4f476e294a93cb
Thomas Sullivan <thomassullivanjames@live.com>
2025-01-07 15:30:39 -0500

    game filter

--- frontend/src/Components/Games/GameCollections/GameCollectionsDisplay.tsx
+++ frontend/src/Components/Games/GameCollections/GameCollectionsDisplay.tsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import { game } from "./GameCollectionsLogic"
 
 type gameProps = {
-    data: game[];
+  data: game[];
 }
 
 function GameCollectionsDisplay({
-    data}: gameProps
+  data }: gameProps
 ) {
-    return (
-        <ul className="games-list-grid">
-            {data.map((game) => {
-                return (
-                    <li key={game.gameId} className="game-tile">
-                        <img src={game.gameImageUrl} alt={game.title}/>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+
+  const [filterString, setFilterString] = useState("");
+
+  return (
+    <>
+      <span>Game filter: </span><input onInput={(e)=>{
+        setFilterString(e.currentTarget.value);
+      }} type="text"></input>
+
+      <ul className="games-list-grid">
+        {
+          data
+            .filter((game) => {return game.title.toLowerCase().includes(filterString); })
+            .map((game) => {
+            return (
+              <li key={game.gameId} className="game-tile">
+                <img src={game.gameImageUrl} alt={game.title} />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </>
+  )
 }
 
 export default GameCollectionsDisplay
