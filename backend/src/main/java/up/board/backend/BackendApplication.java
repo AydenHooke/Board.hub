995998a8fb980c11a528dd5571deeb69a401936e
Thomas Sullivan <thomassullivanjames@live.com>
2025-01-07 10:19:50 -0500

    kafka working

--- backend/src/main/java/up/board/backend/BackendApplication.java
+++ backend/src/main/java/up/board/backend/BackendApplication.java
@@ -1,37 +1,14 @@
 package up.board.backend;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
-  }
-
-  @Bean
-  public NewTopic topic() {
-    return TopicBuilder.name("test")
-        .partitions(10)
-        .replicas(1)
-        .build();
   }
 
 }
