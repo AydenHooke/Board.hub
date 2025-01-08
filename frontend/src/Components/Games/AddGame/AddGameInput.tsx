f5f8c9bf7f8eda6b397e49ac4be0a794fdb65176
Cullen Vaughan <nelluc464@gmail.com>
2025-01-08 16:14:40 -0600

    Added add custom game button

---
+++ frontend/src/Components/Games/AddGame/AddGameInput.tsx
@@ -0,0 +1,73 @@
+type stateProp = {
+    bggId: number, setBggId: React.Dispatch<React.SetStateAction<number>>,
+    gameImageUrl: string, setGameImageUrl: React.Dispatch<React.SetStateAction<string>>,
+    title: string, setTitle: React.Dispatch<React.SetStateAction<string>>,
+    description: string, setDescription: React.Dispatch<React.SetStateAction<string>>,
+    isChecked: boolean, setIsChecked: React.Dispatch<React.SetStateAction<boolean>>,
+    pending: boolean,
+    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+}
+
+function AddGameInput({
+    bggId, setBggId,
+    gameImageUrl, setGameImageUrl,
+    title, setTitle,
+    description, setDescription,
+    isChecked, setIsChecked,
+    pending,
+    handleSubmit}: stateProp
+) {
+ return (
+    <form onSubmit={handleSubmit}>
+        {(!isChecked) && <h2>Add a Custom Game</h2>}
+        {(isChecked) && <h2>Add a Game from BGG</h2>}
+
+        <label>
+            Use BGG ID:
+            <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)}/>
+        </label>
+
+        {(!isChecked) &&
+            <>
+                <br/>
+                <label>
+                    <input required type="text" placeholder="Enter Title of Game" value={title} onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)
+                    }/>
+                </label>
+                <br/>
+                <label>
+                    <input required type="text" placeholder="Enter Description of Game" value={description} onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value)
+                    }/>
+                </label>
+                <br/>
+                <label>
+                    <input type="text" placeholder="Enter a URL of the Game Image" value={gameImageUrl} onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => setGameImageUrl(e.target.value)
+                    }/>
+                </label>
+                <br/>
+            </>
+        }
+
+        {(isChecked) && 
+            <>
+                <br/>
+                <label>
+                    <input required type="number" placeholder="Enter BGG ID" value={`${bggId}`} onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => setBggId(parseInt(e.target.value))
+                    }/>
+                </label>
+                <br/>
+            </>
+        }
+
+        {(pending) && <button className="default-button" type="submit">Add</button>}
+        {(!pending) && <p>request is pending</p>}
+        <br/>
+    </form>
+ )
+}
+
+export default AddGameInput
