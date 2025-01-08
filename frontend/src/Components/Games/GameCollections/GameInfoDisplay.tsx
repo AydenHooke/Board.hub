3233320880aa37e50f23694babc3d2d4b51cac7a
Thomas Sullivan <thomassullivanjames@live.com>
2025-01-08 13:42:20 -0500

    fixed &s

--- frontend/src/Components/Games/GameCollections/GameInfoDisplay.tsx
+++ frontend/src/Components/Games/GameCollections/GameInfoDisplay.tsx
@@ -4,70 +4,86 @@ import { game } from "./GameCollectionsLogic";
 import { useEffect, useState } from "react";
 
 type gameProps = {
-    data: game;
+  data: game;
 }
 
-function GameInfoDisplay({data}: gameProps) {
-    const { id: contextId, jwt: contextJwt } = useAccount();
+function GameInfoDisplay({ data }: gameProps) {
+  const { id: contextId, jwt: contextJwt } = useAccount();
 
-    const [status, setStatus] = useState(-1);
+  const [status, setStatus] = useState(-1);
 
-    useEffect(() => {
-        /*
-        if (contextId != '') {
-            axios
-            .get(`http://localhost:8080/game/CheckGameOwnership`, {
-                headers: {"Authorization" : `${contextJwt}`}
-            })
-            .then((Response) => setStatus(Response.status))
-            .catch((error) => console.error(error));
-        }
-        */
-    }, [])
-
-    function handleSubmitAdd(event: any) {
-        event.preventDefault();
-        
-        /*
+  useEffect(() => {
+    /*
+    if (contextId != '') {
         axios
-            .post(`http://localhost:8080/game/persistOrCollectOneGame`, {
-
-            }, {
-                headers: {"Authorization" : `${contextJwt}`}
-            })
-            .then((Response) => console.log(Response))
-            .catch((error) => console.error(error));
-        */
+        .get(`http://localhost:8080/game/CheckGameOwnership`, {
+            headers: {"Authorization" : `${contextJwt}`}
+        })
+        .then((Response) => setStatus(Response.status))
+        .catch((error) => console.error(error));
     }
+    */
+  }, [])
 
-    function handleSubmitRemove(event: any) {
-        event.preventDefault();
-        
-        /*
-        axios
-            .post(`http://localhost:8080/game/persistOrCollectOneGame`, {
+  function handleSubmitAdd(event: any) {
+    event.preventDefault();
 
-            }, {
-                headers: {"Authorization" : `${contextJwt}`}
-            })
-            .then((Response) => console.log(Response))
-            .catch((error) => console.error(error));
-        */
-    }
+    /*
+    axios
+        .post(`http://localhost:8080/game/persistOrCollectOneGame`, {
 
-    return (
-        <div className="game-info">
-            <h1 style={{display: "inline"}}>{data.title}</h1>
+        }, {
+            headers: {"Authorization" : `${contextJwt}`}
+        })
+        .then((Response) => console.log(Response))
+        .catch((error) => console.error(error));
+    */
+  }
 
-            {((contextId != '') && (status == 204)) && <button style={{float: "right"}} onClick={handleSubmitAdd}>Add Game to your Collection</button>}
-            {((contextId != '') && (status == 200)) && <button style={{float: "right"}} onClick={handleSubmitRemove}>Remove Game from your Collection</button>}
-            <br/>
+  function handleSubmitRemove(event: any) {
+    event.preventDefault();
 
-            <img src={data.gameImageUrl} alt={data.title}/>
-            <p>Rating:{data.rating} Price:{data.price}</p>
-            <p>{data.description}</p>
-        </div>
-    )
+    /*
+    axios
+        .post(`http://localhost:8080/game/persistOrCollectOneGame`, {
+
+        }, {
+            headers: {"Authorization" : `${contextJwt}`}
+        })
+        .then((Response) => console.log(Response))
+        .catch((error) => console.error(error));
+    */
+  }
+
+  function escapeRegExp(string: String) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+  }
+  function replaceAll(str: String, find: any, replace: any) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+  }
+  var desc = data.description;
+  desc = replaceAll(desc, '&amp;', '&');
+  desc = replaceAll(desc, '&#10;&#10;', ' ');
+  desc = replaceAll(desc, '&#10;', '');
+  desc = replaceAll(desc, '&quot;', '"');
+  desc = replaceAll(desc, '&mdash;', '-');
+  desc = replaceAll(desc, '&hellip;', '…');
+  desc = replaceAll(desc, '&nbsp;', ' ');
+
+  return (
+    <div className="game-info">
+      <h1 style={{ display: "inline" }}>{data.title}</h1>
+
+      {((contextId != '') && (status == 204)) && <button style={{ float: "right" }} onClick={handleSubmitAdd}>Add Game to your Collection</button>}
+      {((contextId != '') && (status == 200)) && <button style={{ float: "right" }} onClick={handleSubmitRemove}>Remove Game from your Collection</button>}
+      <br />
+
+      <img src={data.gameImageUrl} alt={data.title} />
+      <p>Rating: {data.rating} </p>
+      <p>Price:{data.price}</p>
+      <p>{desc}</p>
+    </div>
+  )
 }
 
 export default GameInfoDisplay
